4fc617e6713ab914a82fbf37c781fd860210ee01
Maks Rakomin <maks.rakomin@greenice.net>
2025-03-24 14:39:24 +0100

    Add authentication dependencies and user login functionality
    Integrated `@nestjs/passport`, `passport`, and related dependencies to enable authentication. Implemented `findOne` method in `users.service` to retrieve users by email. Added API endpoint to test user login in `rest.clients.http`.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
+import * as bcrypt from 'bcryptjs';
+import { LoginDTO } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async login(loginDTO: LoginDTO): Promise<User> {
+    const user = await this.userService.findOne(loginDTO);
+    const passwordMatched = await bcrypt.compare(
+      loginDTO.password,
+      user.password,
+    );
+    if (passwordMatched) {
+      delete (user as Partial<User>).password;
+      return user;
+    } else {
+      throw new UnauthorizedException('Password does not match');
+    }
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
@@ -17,6 +17,14 @@ export class UsersService {
     userDTO.password = await bcrypt.hash(userDTO.password, salt);
     const user = await this.userRepository.save(userDTO);
     delete (user as Partial<User>).password;
+    return user;
+  }
+
+  async findOne(data: Partial<User>): Promise<User> {
+    const user = await this.userRepository.findOneBy({ email: data.email });
+    if (!user) {
+      throw new UnauthorizedException('Could not find user');
+    }
     return user;
   }
 }
